518e251fdc95be234003134a70d4da31b7078f23
ACharLuk <acharluk@gmail.com>
2014-06-17 20:19:56 +0200

    Created Strings class

---
+++ src/main/java/com/acharluk/voidcraft2/lib/Strings.java
@@ -0,0 +1,12 @@
+package com.acharluk.voidcraft2.lib;
+
+/**
+ * Created by ACharLuk on 17/06/2014.
+ */
+public class Strings {
+
+    public static final String MODID = "voidcraft2";
+    public static final String NAME = "Voidcraft 2";
+    public static final String VERSION = "2.0";
+
+}
